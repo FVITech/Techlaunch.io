1697ec33531ad1ece5332cc9c04f93bde40c85ce
Brenton Cozby <brenton.cozby@gmail.com>
2017-05-17 18:31:34 -0400

    add sections to web developer page

--- web-developer/index.php
+++ web-developer/index.php
@@ -1,21 +1,139 @@
 <?php $page = "web-developer"; include('../parts/head.php'); ?>
 <body>
     <?php include('../parts/navbar.php'); ?>
+
     <section class="above-the-fold">
         <div class="container">
             <img src="../images/logos/techlaunch-blackbg.svg" alt="Techlaunch logo" class="logo">
-            <div class="cta-box">
-                <h2>launch a career in technology</h2>
-                <p>Techlaunch offers programs in Web Development, Web Design, and IT Security. We teach more than short-term coding bootcamps to ensure that you will not only be prepared to get a new career, but will also thrive.</p>
-                <button class="browse-programs">Browse Programs</button>
-                <button class="get-more-info">Get More Info</button>
+            <div class="content">
+                <div class="text-box">
+                    <h2>become a software developer</h2>
+                    <h4>Join the most in-demand profession on the planet.</h4>
+                    <div class="figures-container">
+                        <div class="figure">
+                            <p class="number">$97,990</p>
+                            <p class="label">Median Salary</p>
+                        </div>
+                        <div class="figure">
+                            <p class="number">1,018,000</p>
+                            <p class="label">Job Openings</p>
+                        </div>
+                        <div class="figure">
+                            <p class="number">17%</p>
+                            <p class="label">Annual Growth</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-container">
+                    <h2 class="title">Find out more about Techlaunch</h2>
+                    <p class="subtitle">Get your questions answered and find out if Techlaunch is for you.</p>
+                    <form id="web-dev-form">
+                        <div class="form-input">
+                            <label>name</label>
+                            <input type="text" name="name" placeholder="Enter your name here">
+                        </div>
+                        <div class="form-input">
+                            <label>email</label>
+                            <input type="email" name="email" placeholder="Enter your email here">
+                        </div>
+                        <button type="submit">get info</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="program-overview">
+        <div class="container">
+            <h2 class="section-title">program overview</h2>
+            <p class="subtitle">Techlaunch offers a more robust program than traditional short-term coding boot camps. Come with passion and leave with a valuable skill set you can put to work!</p>
+            <div class="cards-container">
+                <div class="left">
+                    <div class="card">
+                        <img src="/images/icons/atom.svg" alt="">
+                        <p>Full-Stack Web Development</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/icons/calculator.svg" alt="">
+                        <p>36-Week (9-Month) immersive program</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/icons/clipboard.svg" alt="">
+                        <p>Career Placement Assistance</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/icons/statistics.svg" alt="">
+                        <p>Small Class sizes for one-on-one learning</p>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="card">
+                        <img src="/images/icons/clipboard.svg" alt="">
+                        <p>Federal Financial aid available to those who qualify</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/icons/statistics.svg" alt="">
+                        <p>Campus located in Doral, a rapidly growing tech community</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/icons/atom.svg" alt="">
+                        <p>Curriculum designed with local Miami job market in mind</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/icons/calculator.svg" alt="">
+                        <p>Complete the program with a Diploma and a Portfolio of work to show prospective employers</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="students-success">
+        <h2 class="section-title">student's success</h2>
+        <div class="container">
+            <div class="testimonials-container">
+                <div class="testimonial">
+                    <p class="quote">"I’m grateful to FVI and all the professors, like Victor Moreno and Randy Ferrer, for providing me with skills and abilities that helped open the doors to a world full of new opportunities."</p>
+                    <img src="/images/people/frank.jpg" alt="Frank">
+                    <span class="name">Frank Veloz</span>
+                </div>
+                <div class="testimonial">
+                    <p class="quote">"When I started school I didn’t have the slightest idea of how much I would learn. I started the course without knowing anything about healthcare and the care that it requires."</p>
+                    <img src="/images/people/tatiana.png" alt="Tatiana">
+                    <span class="name">Tatiana Torres</span>
+                </div>
+                <div class="testimonial">
+                    <p class="quote">"FVI helped me to accommodate my externship hours around my busy schedule. Today, I am working as a Medical Assistant in a doctor's office, in which I perform EKG, Phlebotomy, Data entry, Vital signs etc."</p>
+                    <img src="/images/people/yanet.png" alt="Yanet">
+                    <span class="name">Yanet Gonzalez</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="job-info">
+        <div class="container">
+            <h2 class="section-title">what kind of job can i get as a graduate?</h2>
+            <p>The <strong>Web Developer Program</strong> from Techlaunch at FVI will prepare graduates to develop practical skills and knowledge needed or entry-level career opportunities in areas involving sotware design and development, systems analysis, programming, process design and management, and other areas related to computer software production and deployment.</p>
+            <p>The fast growth in web and mobile development, combined with the increasing use of software, continues to create <strong>strong demand for software developers</strong>. The career services team at FVI will work with you to provide internships during training and upon graduation assist you in finding a great career as a web developer. FVI maintains strong working relationships with top companies in the area so we can match your passion and ability to career opportunities.</p>
+            <div class="image-section">
+                <div class="left"></div>
+                <div class="right">
+                    <h3>will i obtain any certifications?</h3>
+                    <p>Upon graduation from the <strong>Web Developer Program</strong>, students will receive a diploma. Additionally, students will have a portfolio of work showcasing their abilities in order to aid them in finding full-time jobs or freelance web development contracts.</p>
+                    <button>get info</button>
+                </div>
             </div>
         </div>
     </section>
 
     <section class="hiring-partners">
+        <h2 class="section-title">hiring partners</h2>
         <div class="logos-container">
-            <img src="" alt="">
+            <img src="/images/logos/microsoft.png" alt="Microsoft logo">
+            <img src="/images/logos/google.png" alt="Google logo">
+            <img src="/images/logos/amazon.png" alt="Amazon logo">
+            <img src="/images/logos/facebook.png" alt="Facebook logo">
         </div>
     </section>
 
