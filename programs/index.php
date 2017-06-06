88b3c415df83aea9da186d9f3b66a529ec0ab12c
Brenton Cozby <brenton.cozby@gmail.com>
2017-06-06 14:06:23 -0400

    refine CSS base styles

--- programs/index.php
+++ programs/index.php
@@ -12,18 +12,18 @@
     <section>
         <div class="container programs">
             <div class="program">
-                <div class="image"></div>
-                <h3>web development program</h3>
+                <a href=""><div class="image"></div></a>
+                <h3><a href="">web development program</a></h3>
                 <p><strong>Papo doral flagler literally souwesera flagami booty bass wynwood caja china tiki tiki music.</strong> Croquetica miami MiMo, casa de carajo irregardless wynwood LIV Ultra cafecito que cute miami springs venetian causeway no yeah, yeah no. Miami time vizcaya PubSub Pitbull, pero like besito Azucar downtown UM wynwood collins ave critical mass westchester venetian causeway south beach. UM power 96 leah arts district super la 20 y 20 calle ocho bulykkk supposably dale caja china Deco Drive Heat.</p>
             </div>
             <div class="program">
-                <div class="image"></div>
-                <h3>cyber security network technician</h3>
+                <a href=""><div class="image"></div></a>
+                <h3><a href="">cyber security network technician</a></h3>
                 <p><strong>Papo doral flagler literally souwesera flagami booty bass wynwood caja china tiki tiki music.</strong> Croquetica miami MiMo, casa de carajo irregardless wynwood LIV Ultra cafecito que cute miami springs venetian causeway no yeah, yeah no. Miami time vizcaya PubSub Pitbull, pero like besito Azucar downtown UM wynwood collins ave critical mass westchester venetian causeway south beach. UM power 96 leah arts district super la 20 y 20 calle ocho bulykkk supposably dale caja china Deco Drive Heat.</p>
             </div>
             <div class="program">
-                <div class="image"></div>
-                <h3>web design program</h3>
+                <a href=""><div class="image"></div></a>
+                <h3><a href="">web design program</a></h3>
                 <p><strong>Papo doral flagler literally souwesera flagami booty bass wynwood caja china tiki tiki music.</strong> Croquetica miami MiMo, casa de carajo irregardless wynwood LIV Ultra cafecito que cute miami springs venetian causeway no yeah, yeah no. Miami time vizcaya PubSub Pitbull, pero like besito Azucar downtown UM wynwood collins ave critical mass westchester venetian causeway south beach. UM power 96 leah arts district super la 20 y 20 calle ocho bulykkk supposably dale caja china Deco Drive Heat.</p>
             </div>
         </div>
