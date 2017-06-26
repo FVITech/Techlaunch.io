1050933cb6e90805f8b8d7d715fce592e582d82e
Brenton Cozby <brenton.cozby@gmail.com>
2017-06-26 13:48:02 -0400

    split the about-us sections with images

--- about-us/index.php
+++ about-us/index.php
@@ -11,31 +11,36 @@
         </div>
     </section>
 
-    <section>
+    <section class="content">
         <div class="container">
-            <h2 class="section-title">Leading The Way In Tech Education</h2>
-            <p>In the rapidly growing tech industry, finding the right training is key. At TechLaunch @ Florida Vocational Institute (FVI), we’re committed to preparing our students for a career in technology in less than a year. In this time, we prepare our students to fill the void by providing the training they need. In our programs, we transform general computer users into technologists and empower people to get a rewarding career within one year.</p>
-        </div>
-    </section>
-
-    <section class="bg-gray">
-        <div class="container">
-            <h2 class="section-title">Bridging The Technological Gaps</h2>
-            <p>As you may know, the world of tech presents many challenges, and it can be difficult to keep up with this constantly changing industry. That’s why, at TechLaunch @ FVI we continually refine our curriculum based on feedback from our hiring partners – companies who use technology everyday and can identify skill gaps within the industry. This puts our grads on the cutting-edge of technology and provides them with hard to find skills that employers are looking for.</p>
-        </div>
-    </section>
-
-    <section>
-        <div class="container">
-            <h2 class="section-title">Investing In Our Community</h2>
-            <p>Along with giving our grads new career opportunities, at TechLaunch @ FVI, we’re also committed to helping our community. With programs like TechHire.Miami and Summer Bootcamps, we’re building local awareness of the growing tech field and giving high school students the chance to learn new skills. By exposing these students to the tech industry at a young age, we can create a spark of interest and show them the world of opportunities being created as more companies are adopting technology into their daily operations.</p>
-        </div>
-    </section>
-
-    <section class="bg-gray">
-        <div class="container">
-            <h2 class="section-title">A Better Tomorrow Starts Today</h2>
-            <p>At TechLaunch @ FVI, we’re helping our students match their passions with their skills. Whether they’ve completed high school, have some college experience, or even have a college degree, we can provide the training they need to start a new career in the tech industry in less than a year. Our extensive, hands-on coursework prepares our grads to enter the tech industry with the tools and confidence that appeals to potential employers. And with our career services team, we can help our graduates find tech jobs that allow them to use their skills and grow.</p>
+            <div class="split-2">
+                <div class="split-box left image"></div>
+                <div class="split-box right text">
+                    <h2 class="section-title">Leading The Way In Tech Education</h2>
+                    <p>In the rapidly growing tech industry, finding the right training is key. At TechLaunch @ Florida Vocational Institute (FVI), we’re committed to preparing our students for a career in technology in less than a year. In this time, we prepare our students to fill the void by providing the training they need. In our programs, we transform general computer users into technologists and empower people to get a rewarding career within one year.</p>
+                </div>
+            </div>
+            <div class="split-2">
+                <div class="split-box left text">
+                    <h2 class="section-title">Bridging The Technological Gaps</h2>
+                    <p>As you may know, the world of tech presents many challenges, and it can be difficult to keep up with this constantly changing industry. That’s why, at TechLaunch @ FVI we continually refine our curriculum based on feedback from our hiring partners – companies who use technology everyday and can identify skill gaps within the industry. This puts our grads on the cutting-edge of technology and provides them with hard to find skills that employers are looking for.</p>
+                </div>
+                <div class="split-box right image"></div>
+            </div>
+            <div class="split-2">
+                <div class="split-box left image"></div>
+                <div class="split-box right text">
+                    <h2 class="section-title">Investing In Our Community</h2>
+                    <p>Along with giving our grads new career opportunities, at TechLaunch @ FVI, we’re also committed to helping our community. With programs like TechHire.Miami and Summer Bootcamps, we’re building local awareness of the growing tech field and giving high school students the chance to learn new skills. By exposing these students to the tech industry at a young age, we can create a spark of interest and show them the world of opportunities being created as more companies are adopting technology into their daily operations.</p>
+                </div>
+            </div>
+            <div class="split-2">
+                <div class="split-box left text">
+                    <h2 class="section-title">A Better Tomorrow Starts Today</h2>
+                    <p>At TechLaunch @ FVI, we’re helping our students match their passions with their skills. Whether they’ve completed high school, have some college experience, or even have a college degree, we can provide the training they need to start a new career in the tech industry in less than a year. Our extensive, hands-on coursework prepares our grads to enter the tech industry with the tools and confidence that appeals to potential employers. And with our career services team, we can help our graduates find tech jobs that allow them to use their skills and grow.</p>
+                </div>
+                <div class="split-box right image"></div>
+            </div>
         </div>
     </section>
 
