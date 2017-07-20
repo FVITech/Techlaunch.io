08ebc4b042ce14e61558eec98750d6480ae20a49
Brenton Cozby <brenton.cozby@gmail.com>
2017-07-20 14:58:44 -0400

    Added more student projects

--- parts/student-projects/index.php
+++ parts/student-projects/index.php
@@ -1,20 +1,40 @@
 <section class="student-projects">
     <h2 class="section-title">Student Projects</h2>
     <div class="projects-container">
-        <a class="project" target="_blank" rel="noopener" href="http://aespinet.fvi-grad.com/memory-card-game/">
-            <div class="image"></div>
-            <div class="title">Memory Card Game</div>
+        <a class="project" target="_blank" rel="noopener" href="http://avivero.fvi-grad.com/flappyBird/">
+            <img src="<?php echo $rootPath ?>images/student-projects/flappy-bird.jpg" alt="Flappy Bird Game" />
+            <div class="title">Flappy Bird Game</div>
         </a>
-        <a class="project" target="_blank" rel="noopener" href="http://www.msmadlogistics.com">
-            <div class="image"></div>
-            <div class="title">MSM Ad Logistics</div>
+        <a class="project" target="_blank" rel="noopener" href="http://www.starthubcenters.com">
+            <img src="<?php echo $rootPath ?>images/student-projects/startuphub-miami.jpg" alt="Startuphub Miami" />
+            <div class="title">Startuphub Miami</div>
+        </a>
+        <a class="project" target="_blank" rel="noopener" href="http://velozmontero.fvi-grad.com/peoman/">
+            <img src="<?php echo $rootPath ?>images/student-projects/peoman-game.jpg" alt="Peoman Game" />
+            <div class="title">Peoman Game</div>
+        </a>
+        <a class="project" target="_blank" rel="noopener" href="http://anthonyfalzon-law.com">
+            <img src="<?php echo $rootPath ?>images/student-projects/anthony-falzon.jpg" alt="Anthony Falzon" />
+            <div class="title">Anthony Falzon</div>
+        </a>
+        <a class="project" target="_blank" rel="noopener" href="http://velozmontero.fvi-grad.com/chat/">
+            <img src="<?php echo $rootPath ?>images/student-projects/chat-app.jpg" alt="Chat App" />
+            <div class="title">Chat App</div>
         </a>
         <a class="project" target="_blank" rel="noopener" href="http://ybravo.fvi-grad.com/weather-app/">
-            <div class="image"></div>
+            <img src="<?php echo $rootPath ?>images/student-projects/weather-app.jpg" alt="Weather App" />
             <div class="title">Weather App</div>
         </a>
+        <a class="project" target="_blank" rel="noopener" href="http://www.msmadlogistics.com">
+            <img src="<?php echo $rootPath ?>images/student-projects/msm-ad-logistics.jpg" alt="MSM Ad Logistics" />
+            <div class="title">MSM Ad Logistics</div>
+        </a>
+        <a class="project" target="_blank" rel="noopener" href="http://aespinet.fvi-grad.com/memory-card-game/">
+            <img src="<?php echo $rootPath ?>images/student-projects/memory-card-game.jpg" alt="Memory Card Game" />
+            <div class="title">Memory Card Game</div>
+        </a>
         <a class="project" target="_blank" rel="noopener" href="http://pvegliante.fvi-grad.com/Music-Box/">
-            <div class="image"></div>
+            <img src="<?php echo $rootPath ?>images/student-projects/mini-piano.jpg" alt="Mini Piano" />
             <div class="title">Mini Piano</div>
         </a>
     </div>
