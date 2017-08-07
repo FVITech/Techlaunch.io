69193e72329353cf8a9e320ecddef5ec6203de62
Swolebrain <thecodingteacher@gmail.com>
2017-08-07 12:35:42 -0400

    adding more debug to php hook

--- github/hook.php
+++ github/hook.php
@@ -30,6 +30,8 @@ function run() {
         error_log('Made it past IP check',0);
         foreach ($config['endpoints'] as $endpoint) {
             // check if the push came from the right repository and branch
+            error_log("payload->ref: " . $payload->ref);
+            error_log("endpoint.branch: " . $endpoint['branch']);
             if ($payload->repository->url == 'https://github.com/' . $endpoint['repo']
                 && $payload->ref == 'refs/heads/' . $endpoint['branch']) {
                 // execute update script, and record its output
