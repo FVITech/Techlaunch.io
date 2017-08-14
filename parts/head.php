ea88111ab441ec9c724b57c07b08d074e16225f6
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-14 16:02:55 -0400

    add favicons

--- parts/head.php
+++ parts/head.php
@@ -15,6 +15,23 @@
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:description" content="<?php echo $meta_desc ?>" />
     <meta name="twitter:title" content="<?php echo $meta_title ?>" />
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $rootPath ?>images/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $rootPath ?>images/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $rootPath ?>images/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $rootPath ?>images/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $rootPath ?>images/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $rootPath ?>images/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $rootPath ?>images/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $rootPath ?>images/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $rootPath ?>images/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $rootPath ?>images/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $rootPath ?>images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $rootPath ?>images/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $rootPath ?>images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $rootPath ?>manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?php echo $rootPath ?>images/favicons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="<?php echo $rootPath ?>vendor/font-awesome-4.7.0/css/font-awesome.min.css" async>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800">
     <link rel="stylesheet" href="<?php echo $rootPath ?>css/<?php echo $page ?>/index.css">
