574c04c0bc64bf2df8f5e1026606a18aa9929538
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-22 23:52:23 -0400

    try to enable php errors

--- events/get-location.php
+++ events/get-location.php
@@ -3,9 +3,11 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+    $venue_id = $_GET['venue_id'];
+
     require_once('../eventbrite-sdk-php/HttpClient.php');
     $client = new HttpClient('M43W3J65BLAWDNGL2KJY&order_by=start_asc');
-    $response = $client->get('/users/me/owned_events');
+    $response = $client->get("/venues/$venue_id");
 
     echo json_encode($response);
 ?>
--- events/index.php
+++ events/index.php
@@ -24,6 +24,7 @@ include('../parts/head.php'); ?>
     <section class="events-list" id="events-list">
         <div class="container">
             <p class="section-summary">Techlaunch regularly hosts various events, including free coding workshops and informational sessions.</p>
+            <br>
             <div class="events-list-container" id="events-list-container">
                 <br>
                 <h3 style="text-align: center">Loading events...</h3>
