52dfb7024d45560e580d87cfbee4c419d3e7d496
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-30 12:41:26 -0400

    Fix: set hiring-partners form to send to cvazquez@fvi.edu

--- hiring-partners/contact.php
+++ hiring-partners/contact.php
@@ -8,7 +8,7 @@
     $website = strip_tags(htmlspecialchars($_POST['website']));
     $message = strip_tags(htmlspecialchars($_POST['message']));
     // Create the email and send the message
-    $to = 'Hiringpartners@techlaunch.io'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'cvazquez@fvi.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Hiring Partners Form:  $subject";
     $email_body = "You have received a new message from the Hiring Partners form.\n\n"."Here are the details:\n\nFirst Name: $fname\n\nLast Name: $lname\n\nEmail: $email_address\n\nPhone: $phone\n\nCompany: $company\n\nLogo: $logo\n\nWebsite: $website\n\nMessage:\n$message";
     $headers = "From: hiring-partners-form@techlaunch.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
