fe4eba1dd2995a790e5fb6b35b5c43fcd07f8550
Brenton Cozby <brenton.cozby@gmail.com>
2018-02-12 20:26:51 -0500

    add campus dropdown to get-info-form

--- parts/get-info-form.php
+++ parts/get-info-form.php
@@ -41,14 +41,13 @@
         ?>
         <i class="close-btn fa fa-times-circle" aria-hidden="true"></i>
         <form method="POST" action="<?= $form_action; ?>">
-            <input type="hidden" name="campus" value="miami">
-            <!-- <div class="form-input">
+            <div class="form-input">
                 <label>Preferred Location</label>
                 <select name="campus" id="campus">
                     <option value="Miami" <?php if(isset($campus) && $campus == 'miami') echo 'selected' ?>>Miami Campus</option>
                     <option value="Miramar" <?php if(isset($campus) && $campus == 'miramar') echo 'selected' ?>>Miramar Campus</option>
                 </select>
-            </div> -->
+            </div>
             <div class="form-input">
                 <label>Program of Interest</label>
                 <select name="program_name" id="program_name">
