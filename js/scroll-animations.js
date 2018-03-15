88972f00246d8e1a0f62090b589ec480b714dbc2
Brenton Cozby <brenton.cozby@gmail.com>
2018-03-15 10:24:37 -0400

    fix scroll animations

--- js/scroll-animations.js
+++ js/scroll-animations.js
@@ -1,82 +1,92 @@
 const debounce = require('lodash.debounce')
+const throttle = require('lodash.throttle')
 const { switchQuote } = require('./quotes.js')
 
 // Add selectors here and they will all have the class 'scroll-visible'
 // added to them when they scroll into view
-let selectors = [
-    $('.graph'),
-    $('.appear')
+const selectors = [
+    document.querySelectorAll('.appear'),
+    document.querySelectorAll('.graph'),
 ]
 
 let animElements = []
+let windowHeight = null
+let lastWindowScrollY = 0
+let offset = 150
 
-function _populateElements() {
+const supportPageOffset = (window.pageXOffset !== undefined)
+const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat')
+
+const getWindowScrollY = () => {
+    if (supportPageOffset) {
+        return window.pageYOffset
+    } else if (isCSS1Compat) {
+        return document.documentElement.scrollTop
+    }
+
+    return document.body.scrollTop
+}
+
+function getPositions() {
     animElements = []
 
-    selectors.forEach(function($selector) {
-        $selector.each(function(i, el) {
-            animElements.push({element: $(el), position: null})
-        })
-    })
-}
-
-function _getPositions() {
-    _populateElements()
-
-    animElements.forEach(function($el) {
-        $el.position = $el.element.offset().top
-    })
-}
-
-const supportPageOffset = window.pageXOffset !== undefined;
-const isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-const windowScroll = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
-let windowScrollCache
-let windowHeight
-let offset
-
-function playAnimations() {
-    animElements.forEach(function(el) {
-        const triggerPoint = el.position - windowHeight + offset
-        if(window.scrollY > triggerPoint) {
-            el.element.addClass('scroll-visible')
-        } else {
-            el.element.removeClass('scroll-visible')
+    selectors.forEach((selector) => {
+        if (selector) {
+            selector.forEach((element) => {
+                animElements.push({
+                    element,
+                    position: element.getBoundingClientRect().top + getWindowScrollY(),
+                })
+            })
         }
     })
 }
 
-const $quotesSection = $('section#quotes')
+function playAnimations() {
+    animElements.forEach((el) => {
+        const triggerPoint = Number(el.position) + Number(-windowHeight) + Number(offset)
 
-function _showItemsInView() {
-    if (windowScrollCache === windowScroll) {
-        return
-    }
+        if (lastWindowScrollY > triggerPoint) {
+            el.element.classList.add('scroll-visible')
+        } else {
+            el.element.classList.remove('scroll-visible')
+        }
+    })
+}
 
-    windowScrollCache = windowScroll
-    windowHeight = $(window).height()
-    offset = windowHeight * .1
-    _getPositions()
+function showItemsInView() {
+    windowHeight = window.innerHeight
+    offset = windowHeight * 0.1
+    lastWindowScrollY = getWindowScrollY()
+    getPositions()
     playAnimations()
 }
 
-$(document).ready(function() {
-    // make sure items in view when page loads become visible
-    setTimeout(_showItemsInView, 1000)
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+        fn()
+    } else {
+        document.on('DOMContentLoaded', fn)
+    }
+}
 
-    $(document).resize(debounce(function () {
-        setTimeout(function () {
-            _showItemsInView()
-        }, 200)
-    }), 100)
+ready(() => {
+    // make sure items that are in view when page loads become visible
+    setTimeout(showItemsInView, 1000)
 
-    $(document).scroll(debounce(function () {
+    document.on('resize', debounce(() => {
+        showItemsInView()
+    }, 100))
+
+    const $quotesSection = $('section#quotes')
+
+    document.on('scroll', throttle(() => {
+        lastWindowScrollY = getWindowScrollY()
+
         playAnimations()
 
         if ($quotesSection.length) {
             switchQuote()
         }
-    }, 100))
+    }, 100, { leading: true }))
 })
-
-module.exports.playAnimations = playAnimations
