f1840af003a8cbc79821becb61693e12313981a3
Brenton Cozby <brenton.cozby@gmail.com>
2018-03-21 15:47:00 -0400

    replace content; add new icons and logos

---
+++ web-design-and-digital-marketing/index.php
@@ -0,0 +1,230 @@
+<?php $rootPath = '../'; 
+$page = "web-design-and-digital-marketing"; 
+$meta_title="Miami Web Design and Digital Markting Training Course";
+$meta_key="";
+$meta_desc="As a Web Designer and Digital Marketing professional, you’ll be able create, manage, maintain and market web sites and digital marketing strategies. You will be able to utilize a variety of web design methods and tools from HTML, CSS and Javascript to Adobe, Ajax and more – both frontend and backend programming. On the marketing side you’ll manage social media, SEM, SEO and overall digital marketing strategies.";
+include('../parts/head.php'); ?>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DZRJ7D"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php include('../parts/navbar.php'); ?>
+    <?php $program = "web-design-and-digital-marketing"; $classes = ""; include('../parts/get-info-form.php'); ?>
+
+    <section class="above-the-fold">
+        <div class="container">
+            <a href="<?= $rootPath ?>">
+                <img src="<?= $rootPath ?>images/logos/techlaunch_at_fvi_vertical_dark_bg.png" alt="Techlaunch at Florida Vocational Institute logo" class="logo">
+            </a>
+            <div class="content">
+                <div class="text-box">
+                    <h2>
+                        <span class="small">BECOME A</span>
+                        <br>
+                        Web Design and
+                        <br>
+                        Digital Marketing
+                        <br>
+                        Professional
+                    </h2>
+                </div>
+                <?php
+                    $classes = "on-page";
+                    $program = "web-design-and-digital-marketing";
+                    include('../parts/get-info-form.php');
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <section class="program-overview">
+        <div class="container">
+            <h2 class="section-title appear">Program Overview</h2>
+            <div class="split-3">
+                <p class="split-box subtitle appear">Our in-person Web Design and Digital Marketing program is designed for busy adults who need flexible class schedules and are driven to boost their professional skills or change their career.</p>
+                <p class="split-box subtitle appear">Few programs like this offer the scope of material that we do. Our curriculum is designed to be completed in a dense, but realistic time frame of nine months and includes 9 courses in total. You will gain expert training in both the technical aspects of web development and business-focused digital marketing.</p>
+                <p class="split-box subtitle appear">By the end of the 7-month program, each student will be able to understand how to design, create and maintain websites as well as how to promote a web presence to meet a variety of business objectives.</p>
+            </div>
+            <div class="cards-container">
+                <div class="left">
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/computer-monitor.png" alt="computer-monitor">
+                        <p>Front and Back-End Web Development</p>
+                    </div>
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/diploma.png" alt="diploma">
+                        <p>27-Week (7-Month) immersive program</p>
+                    </div>
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/suit.png" alt="suit">
+                        <p>Career Placement Assistance</p>
+                    </div>
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/two-people.png" alt="two-people">
+                        <p>Small Class sizes for one-on-one learning</p>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/piggy-bank.png" alt="piggy-bank">
+                        <p>Federal Financial aid available to those who qualify</p>
+                    </div>
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/laptop.png" alt="growing-graph">
+                        <p>Convenient campus locations</p>
+                    </div>
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/search-the-globe.png" alt="search-the-globe">
+                        <p>Curriculum and Career Services designed with the South Florida job market in mind</p>
+                    </div>
+                    <div class="card appear">
+                        <img class="lazyload" data-src="<?= $rootPath ?>images/icons/graduate.png" alt="graduate">
+                        <p>Complete the program with a Diploma and a Portfolio of work to show prospective employers</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="students-success">
+        <h2 class="section-title appear">Student Success</h2>
+        <div class="container">
+            <div class="testimonials-container">
+                <div class="testimonial appear">
+                    <p class="quote">"I have so much to thank the school and their teachers for. I thought that becoming a programmer was something that was unachievable for me, but thanks to the instructors, I was able to do so easily."</p>
+                    <img class="lazyload" data-src="<?= $rootPath ?>images/people/yasiel-sanchez-caleo.jpg" alt="Yasiel Sanchez Caleo">
+                    <span class="name">Yasiel Sanchez Caleo</span>
+                </div>
+                <div class="testimonial appear delay-200">
+                    <p class="quote">"TechLaunch demonstrated to me that no matter when you decide, great things can happen if you try. I've acquired knowledge every day, and every single minute I've spent inside of their classroom has made me better."</p>
+                    <img class="lazyload" data-src="<?= $rootPath ?>images/people/alan-espinet.jpg" alt="Alan Espinet Lluvet">
+                    <span class="name">Alan Espinet Lluvet</span>
+                </div>
+                <div class="testimonial appear delay-400">
+                    <p class="quote">"Before I started at TechLaunch I was concerned. I thought that coding was difficult and I didn’t know what to expect. Now I feel that it is easier than everybody thinks."</p>
+                    <img class="lazyload" data-src="<?= $rootPath ?>images/people/lily-cantillo.jpg" alt="Lilianne Cantillo">
+                    <span class="name">Lilianne Cantillo</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="job-info">
+        <div class="container">
+            <h2 class="section-title appear">What kind of job can I get as a graduate?</h2>
+            <div class="split">
+                <p class="appear">The Web Design and Digital Marketing program from Techlaunch will prepare graduates to develop practical skills and knowledge needed for career opportunities in areas involving website design and development and digital marketing. These jobs include <strong>design principles, site implementation, social media marketing, SEO, SEM and marketing messaging and strategies</strong>.</p>
+                <p class="appear">The career services team at TechLaunch work to guide you in finding internship opportunities during training and upon graduation assist you in finding a great career as a digital marketer and web designer.</p>
+                <p class="appear">Techlaunch maintains strong working relationships with top companies in the area so we can match your passion and ability to career opportunities.</p>
+            </div>
+            <div class="image-section">
+                <div class="left appear lazyload" data-bg="<?= $rootPath ?>images/people/guy-on-laptop.jpeg"></div>
+                <div class="right">
+                    <h3 class="appear">Build a Portfolio</h3>
+                    <p class="appear">Upon graduation from the Web Design and Digital Marketing progra, students will receive a diploma. Additionally, students will have a portfolio of work showcasing their abilities to aid them in finding full-time jobs or freelance web development contracts.</p>
+                    <button data-form-program="web-design-and-digital-marketing" class="get-more-info appear">get info</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="program-courses">
+        <div class="container">
+            <h2 class="section-title appear">The Program Includes the Following Courses</h2>
+            <div class="content">
+                <div class="cards-container">
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/icons/digital-marketing-design.svg" alt="Design Principles">
+                        <p>Design Principles</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/logos/html5-logo.png" alt="HTML5">
+                        <p>Basic Front End Programing (JavaScript, HTML5, CSS)</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/icons/digital-marketing-programming.svg" alt="Frontend Programming">
+                        <p>Basic Front End Programming II</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/icons/digital-marketing-structure.svg" alt="Front End Structure">
+                        <p>Intermediate Front End Structure</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/icons/digital-marketing-styling.svg" alt="Styling Techniques">
+                        <p>Intermediate Styling Techniques</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/logos/wordpress-w-logo.png" alt="WordPress">
+                        <p>Intro to PHP and WordPress</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/icons/digital-marketing-user-experience.svg" alt="UX/UI Design">
+                        <p>UI/UX Design</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/icons/digital-marketing-business.svg" alt="Digital Marketing Techniques">
+                        <p>Digital Marketing Techniques, Strategies, and Analysis</p>
+                    </div>
+                    <div class="card">
+                        <img class="logo lazyload" data-src="<?= $rootPath ?>images/icons/digital-marketing-people.svg" alt="Webpack 2 logo">
+                        <p>Effective Engagement in Digital Marketing</p>
+                    </div>
+                </div>
+                <div class="image-container">
+                    <img class="lazyload" data-srcset="<?= $rootPath ?>images/people/group-at-pipeline-mobile.jpg 440w, <?= $rootPath ?>images/people/group-at-pipeline.jpg 1000w" data-sizes="auto" alt="Techlaunch students">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="our-instructors">
+        <div class="container">
+            <div class="content">
+                <div class="left">
+                    <h2 class="section-title appear">Our Instructors</h2>
+                    <p class="appear">Our instructors take a <strong>hands-on</strong> approach and work closely with you to ensure that you gain the necessary skills to succeed. Our students will learn how to <strong>work in teams</strong> using software and tools that are commonly employed in software companies.</p>
+                    <button data-form-program="web-design-and-digital-marketing" class="get-more-info appear">get info</button>
+                </div>
+                <div class="right lazyload" data-bg="<?= $rootPath ?>images/backgrounds/teaching-mobile.jpg"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="our-campus">
+        <div class="container">
+            <div class="split-2">
+                <div class="split-box image-container">
+                    <div class="image appear first lazyload" data-bgset="<?= $rootPath ?>images/pipeline/workspace-mobile.jpg [--small] | <?= $rootPath ?>images/pipeline/workspace.jpg [--large]"></div>
+                </div>
+                <div class="split-box text">
+                    <h2 class="section-title appear">Our Campus</h2>
+                    <p>We are located at the Pipeline Doral co-working space. The Doral area is home to numerous tech companies.</p>
+                </div>
+                <div class="split-box image-container">
+                    <div class="image appear second lazyload" data-bgset="<?= $rootPath ?>images/pipeline/students-3-mobile.jpg [--small] | <?= $rootPath ?>images/pipeline/students-3.jpg [--large]"></div>
+                </div>
+                <div class="split-box image-container">
+                    <div class="image appear delay-300 third lazyload" data-bgset="<?= $rootPath ?>images/pipeline/learning-1-mobile.jpg [--small] | <?= $rootPath ?>images/pipeline/learning-1.jpg [--large]"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="hiring-partners">
+        <h2 class="section-title appear">Hiring Partners</h2>
+        <div class="logos-container">
+            <img class="lazyload" data-src="https://techlaunch.io/blog/wp-content/uploads/2017/07/coach-hq-1-1.png" alt="Coach HQ">
+            <img class="lazyload" data-src="https://techlaunch.io/blog/wp-content/uploads/2017/07/logo-spiderboost-2.png" alt="Spider Boost">
+            <img class="lazyload" data-src="https://techlaunch.io/blog/wp-content/uploads/2017/07/Starthub_MainLogoFile-2.png" alt="Start Hub">
+            <img class="lazyload" data-src="https://techlaunch.io/blog/wp-content/uploads/2017/07/tango_mango_logo.jpg-1.png" alt="Tango Mango">
+            <img class="lazyload" data-src="https://techlaunch.io/blog/wp-content/uploads/2017/07/the-fitz-solution-2.png" alt="Fitz Solution">
+            <img class="lazyload" data-src="https://techlaunch.io/blog/wp-content/uploads/2017/07/WhereByUs-2.pngfinal-2.png" alt="Where by us">
+        </div>
+    </section>
+
+    <?php $rootPath = '../'; include('../parts/footer.php') ?>
+    <?php $rootPath = '../'; include('../parts/bottom.php') ?>
+
+</body>
+</html>
