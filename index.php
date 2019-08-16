e8df67dcb7c1c28fcd651682b80ced5e01d1271a
ebrodienuovometo <ebrodie@nuovometo.com>
2019-08-16 16:44:17 +0500

    tert

--- index.php
+++ index.php
@@ -186,7 +186,12 @@ include('parts/head.php'); ?>
 
     <section class="stacks">
         <h2 class="section-title">Director of TechLaunch Education</h2>
-        <div class="stacks-container">
+        <div class="stacks-container custom-if">
+        <style>
+		@media only screen and (max-width: 480px) {
+.custom-if iframe{ width:100%; height:100%;}
+}
+		</style>
         <h1>
                     <iframe style="width: 532px;height: 300px;" class="lazyload" data-src="https://www.youtube.com/embed/umw2N9SerDs?ecver=2<?= $options ?>" frameborder="0"></iframe>
              </h1>
