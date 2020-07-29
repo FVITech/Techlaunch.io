4fb4ca418790b22b3a0d924a7dcc4de9d4ed570b
Salvi Pascual <salvi.pascual@gmail.com>
2020-07-29 11:56:20 -0400

    add about question to form

--- contact-us/index.php
+++ contact-us/index.php
@@ -5,111 +5,128 @@ $meta_key="";
 $meta_desc="";
 include('../parts/head.php'); ?>
 <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DZRJ7D"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    <?php include('../parts/navbar.php'); ?>
-    <?php $classes = ""; include('../parts/get-info-form.php'); ?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DZRJ7D"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<?php include('../parts/navbar.php'); ?>
+	<?php $classes = ""; include('../parts/get-info-form.php'); ?>
 
-    <section class="above-the-fold">
-        <div class="container">
-            <a href="<?= $rootPath ?>">
-                <img src="<?= $rootPath ?>images/logos/techlaunch_at_fvi_vertical_dark_bg.png" alt="FVI School of Technology logo" class="logo">
-            </a>
-            <br>
-            <h1 class="page-title">Location</h1>
+	<section class="above-the-fold">
+		<div class="container">
+			<a href="<?= $rootPath ?>">
+				<img src="<?= $rootPath ?>images/logos/techlaunch_at_fvi_vertical_dark_bg.png" alt="FVI School of Technology logo" class="logo">
+			</a>
+			<br>
+			<h1 class="page-title">Location</h1>
 <!--            <p class="page-subtitle">FVI School of Technology is is conveniently located in heart of the Doral Business District, inside Pipeline Workspaces. Situated just steps away from CityPlace Doral with easy access from expressway and many parking spaces available.</p>
 -->        </div>
-    </section>
+	</section>
 
-    <section class="request-more-info">
-        <div class="main">
-            <h2 class="section-title">Request More Information</h2>
-            <form method="POST" action="<?= $form_action; ?>">
-            <div class="form-input split-2">
-            <div class="split-boxt">
-                <label>Preferred Location</label>
-                <select name="campus" id="campus" style="width: 100%;">
-                    <!--<option value="Miami" <?php // if ($campus == 'miami' || $campus == '') echo 'selected' ?>>Miami</option>-->
-                    <option value="Miramar" <?php if( $campus == 'miramar') echo 'selected' ?>>Miramar</option>
-                </select>
-            </div>
-            <div class="split-box">
-                <label>Program of Interest</label>
-                <select name="program_name" id="program_name" style="width: 100%;">
-                    <option value="--" disabled <?php if (!isset($program) || $program == '') echo 'selected' ?>>-- Select --</option>
-                    <!--<option value="Web Developer" <?php // if(isset($program) && $program == 'web-developer') echo 'selected' ?>>Web Developer</option>-->
-                    <option value="Cyber Security Network Technician" <?php if(isset($program) && $program == 'cyber-security') echo 'selected' ?>>Cyber Security and Network Technician</option>
-                    <!--<option value="Web Design and Digital Marketing" <?php // if(isset($program) && $program == 'web-design-and-digital-marketing') echo 'selected' ?>>Web Design and Digital Marketing</option>-->
-                   <!-- <option value="aws" <?php // if(isset($program) && $program == 'aws') echo 'selected' ?>>Amazon Web Services</option>-->
-                    <!-- <option value="wordpress" <?php if(isset($program) && $program == 'wordpress') echo 'selected' ?>>WordPress</option> -->
-                </select>
-                <input type="hidden" name="program_id" id="legacy_program" value="<?php if(isset($program)) echo $program ?>" />
-            </div>
-            </div>
-            <div class="form-input split-2">
-                <div class="split-box">
-                    <label>first name</label>
-                    <input type="text" name="first_name" required>
-                </div>
-                <div class="split-box">
-                    <label>last name</label>
-                    <input type="text" name="last_name" required>
-                </div>
-            </div>
-            <div class="form-input split-2">
-                <div class="split-box">
-                    <label>phone</label>
-                    <input type="tel" name="day_phone" required>
-                </div>
-                <div class="split-box">
-                    <label>zip code</label>
-                    <input type="number" name="zipcode">
-                </div>
-            </div>
-            <div class="form-input">
-                <label>email</label>
-                <input type="email" name="email" required>
-            </div>
-            <button class="submit-btn" type="submit">get info</button>
-            <div class="user-feedback"></div>
-        </form>
-            <p class="disclosure">By clicking "Submit Info" above, you agree for FVI School of Technology to contact you using our autodialer, text, pre-recorded message, via telephone or mobile device (including SMS and MMS) and email, even if your number is currently listed on any state, federal or corporate "Do Not Call list".</p>
-        </div>
-        <aside>
-            <h3>Contact Us</h3>
-            <p>So now that you are ready to take that first step to changing your life, come meet with one of our admissions representatives and enroll today! Our admissions process is simple and easy to get you started on the path to success.</p>
-            <h4>Classroom Location:</h4>
-            <a  class="address-link" target="_blank" rel="noopener" href="https://goo.gl/maps/p6Cj7d8PLaS2">
-                <address>
-                    Miramar Campus<br>
-                    3520 Enterprise Way,<br>
-                    Miramar, FL 33025<br>
-                </address>
-            </a>
-            <br>
+	<section class="request-more-info">
+		<div class="main">
+			<h2 class="section-title">Request More Information</h2>
+			<form method="POST" action="<?= $form_action; ?>">
+			<div class="form-input split-2">
+			<div class="split-boxt">
+				<label>Preferred Location</label>
+				<select name="campus" id="campus" style="width: 100%;">
+					<!--<option value="Miami" <?php // if ($campus == 'miami' || $campus == '') echo 'selected' ?>>Miami</option>-->
+					<option value="Miramar" <?php if( $campus == 'miramar') echo 'selected' ?>>Miramar</option>
+				</select>
+			</div>
+			<div class="split-box">
+				<label>Program of Interest</label>
+				<select name="program_name" id="program_name" style="width: 100%;">
+					<option value="--" disabled <?php if (!isset($program) || $program == '') echo 'selected' ?>>-- Select --</option>
+					<!--<option value="Web Developer" <?php // if(isset($program) && $program == 'web-developer') echo 'selected' ?>>Web Developer</option>-->
+					<option value="Cyber Security Network Technician" <?php if(isset($program) && $program == 'cyber-security') echo 'selected' ?>>Cyber Security and Network Technician</option>
+					<!--<option value="Web Design and Digital Marketing" <?php // if(isset($program) && $program == 'web-design-and-digital-marketing') echo 'selected' ?>>Web Design and Digital Marketing</option>-->
+				   <!-- <option value="aws" <?php // if(isset($program) && $program == 'aws') echo 'selected' ?>>Amazon Web Services</option>-->
+					<!-- <option value="wordpress" <?php if(isset($program) && $program == 'wordpress') echo 'selected' ?>>WordPress</option> -->
+				</select>
+				<input type="hidden" name="program_id" id="legacy_program" value="<?php if(isset($program)) echo $program ?>" />
+			</div>
+			</div>
+			<div class="form-input split-2">
+				<div class="split-box">
+					<label>first name</label>
+					<input type="text" name="first_name" required>
+				</div>
+				<div class="split-box">
+					<label>last name</label>
+					<input type="text" name="last_name" required>
+				</div>
+			</div>
+			<div class="form-input split-2">
+				<div class="split-box">
+					<label>phone</label>
+					<input type="tel" name="day_phone" required>
+				</div>
+				<div class="split-box">
+					<label>zip code</label>
+					<input type="number" name="zipcode">
+				</div>
+			</div>
+			<div class="form-input">
+				<label>email</label>
+				<input type="email" name="email" required>
+			</div>
+			<div class="form-input">
+				<label for="custom_field_8">How Did You Hear About Us</label>
+				<select name="custom_field_8" id="custom_field_8" style="width: 100%;">
+					<option value="event">Event</option>
+					<option value="facebook">Facebook</option>
+					<option value="friend-family">Friend/Family Member</option>
+					<option value="google">Google</option>
+					<option value="hulu">Hulu</option>
+					<option value="instagram">Instagram</option>
+					<option value="pinterest">Pinterest</option>
+					<option value="radio">Radio</option>
+					<option value="tv">TV</option>
+					<option value="twitter">Twitter</option>
+					<option value="youtube">YouTube</option>
+					<option value="other">Other</option>
+				</select>
+			</div>
+			<button class="submit-btn" type="submit">get info</button>
+			<div class="user-feedback"></div>
+		</form>
+			<p class="disclosure">By clicking "Submit Info" above, you agree for FVI School of Technology to contact you using our autodialer, text, pre-recorded message, via telephone or mobile device (including SMS and MMS) and email, even if your number is currently listed on any state, federal or corporate "Do Not Call list".</p>
+		</div>
+		<aside>
+			<h3>Contact Us</h3>
+			<p>So now that you are ready to take that first step to changing your life, come meet with one of our admissions representatives and enroll today! Our admissions process is simple and easy to get you started on the path to success.</p>
+			<h4>Classroom Location:</h4>
+			<a  class="address-link" target="_blank" rel="noopener" href="https://goo.gl/maps/p6Cj7d8PLaS2">
+				<address>
+					Miramar Campus<br>
+					3520 Enterprise Way,<br>
+					Miramar, FL 33025<br>
+				</address>
+			</a>
+			<br>
 <!--            <p class="contact"><strong>Email:</strong> <a href="mailto:info@fvi.edu">info@techlaunch.io</a></p>
-           <p class="contact"><strong>Phone:</strong> <a href="tel:786-574-9511">305-783-3599</a></p>
+		   <p class="contact"><strong>Phone:</strong> <a href="tel:786-574-9511">305-783-3599</a></p>
 -->            <p class="contact"><strong>Phone:</strong> <a href="tel:305-783-359">305-783-3599</a></p>
-            <img class="lazyload" data-src="<?= $rootPath ?>images/people/group-at-pipeline-mobile.jpg" alt="FVI School of Technology Students at Pipeline Doral">
-        </aside>
-    </section>
+			<img class="lazyload" data-src="<?= $rootPath ?>images/people/group-at-pipeline-mobile.jpg" alt="FVI School of Technology Students at Pipeline Doral">
+		</aside>
+	</section>
 
-    <section class="images-slider">
-        <div class="carousel">
-            <div id="left-btn" class="left-btn">‹</div>
-            <div id="right-btn" class="right-btn">›</div>
-            <div class="dots"></div>
-            <div class="carousel-item image-1 lazyload" data-bg="<?= $rootPath ?>images/pipeline/learning-1.jpg"></div>
-            <div class="carousel-item image-2 lazyload" data-bg="<?= $rootPath ?>images/pipeline/students-2.jpg"></div>
-            <div class="carousel-item image-3 lazyload" data-bg="<?= $rootPath ?>images/pipeline/teaching-1.jpg"></div>
-            <div class="carousel-item image-4 lazyload" data-bg="<?= $rootPath ?>images/pipeline/workspace.jpg"></div>
-        </div>
-    </section>
+	<section class="images-slider">
+		<div class="carousel">
+			<div id="left-btn" class="left-btn">‹</div>
+			<div id="right-btn" class="right-btn">›</div>
+			<div class="dots"></div>
+			<div class="carousel-item image-1 lazyload" data-bg="<?= $rootPath ?>images/pipeline/learning-1.jpg"></div>
+			<div class="carousel-item image-2 lazyload" data-bg="<?= $rootPath ?>images/pipeline/students-2.jpg"></div>
+			<div class="carousel-item image-3 lazyload" data-bg="<?= $rootPath ?>images/pipeline/teaching-1.jpg"></div>
+			<div class="carousel-item image-4 lazyload" data-bg="<?= $rootPath ?>images/pipeline/workspace.jpg"></div>
+		</div>
+	</section>
 
-    <?php $rootPath = '../'; include('../parts/footer.php') ?>
-    <?php $rootPath = '../'; include('../parts/bottom.php') ?>
+	<?php $rootPath = '../'; include('../parts/footer.php') ?>
+	<?php $rootPath = '../'; include('../parts/bottom.php') ?>
 
 </body>
 </html>
--- parts/get-info-form.php
+++ parts/get-info-form.php
@@ -1,110 +1,126 @@
 <div class="get-info-form <?= $classes ?>">
-    <div class="get-info-form-overlay"></div>
-    <div class="form-container">
-        <h2 class="title">Learn how to get started</h2>
-        <?php
-          $url = '';
-          $escaped_url = 'unknown';
-          if (isset($_SERVER['HTTP_HOST'])){
-            $url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            $escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
-          }
-          $form_action = "https://secure.velocify.com/Import.aspx?Provider=FloridaVocationalInstitute&Client=30010&XmlResponse=True&CampaignId=1093&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
-          
-          $campus = '';
-          if ( isset($_GET['location'])){
-            $campus = $_GET['location'];
-          }
-          
-          if (!isset($origin)){
-            //this is just hardening the script, since $origin is set in navbar.php
-            $origin = "no-origin";
-            if (isset($_GET['origin'])){
-              $origin = $_GET['origin'];
-            }
-            if (isset($_SESSION) && isset($_SESSION['origin'])){
-              $origin = $_SESSION['origin'];
-            }
-          }
+	<div class="get-info-form-overlay"></div>
+	<div class="form-container">
+		<h2 class="title">Learn how to get started</h2>
+		<?php
+			$url = '';
+			$escaped_url = 'unknown';
+			if (isset($_SERVER['HTTP_HOST'])){
+				$url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+				$escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
+			}
+			$form_action = "https://secure.velocify.com/Import.aspx?Provider=FloridaVocationalInstitute&Client=30010&XmlResponse=True&CampaignId=1093&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
+			
+			$campus = '';
+			if ( isset($_GET['location'])){
+				$campus = $_GET['location'];
+			}
+			
+			if (!isset($origin)){
+				//this is just hardening the script, since $origin is set in navbar.php
+				$origin = "no-origin";
+				if (isset($_GET['origin'])){
+					$origin = $_GET['origin'];
+				}
+				if (isset($_SESSION) && isset($_SESSION['origin'])){
+					$origin = $_SESSION['origin'];
+				}
+			}
 
-          if ($origin == 'bing'){
-            $form_action = "https://secure.velocify.com/Import.aspx?Provider=YahooBingPPC&Client=30010&CampaignId=1065&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
-          }
-          elseif ($origin == 'es-bing'){
-            $form_action = "https://secure.velocify.com/Import.aspx?Provider=YahooBingPPCSpanish&Client=30010&CampaignId=1083&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
-          }
-          elseif ($origin == 'facebook'){
-            $form_action = "https://secure.velocify.com/Import.aspx?Provider=FacebookAds&Client=30010&CampaignId=1059&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
-          }
-          elseif ($origin == 'coursereport'){
-            $form_action = "https://secure.velocify.com/Import.aspx?Provider=CourseReport&Client=30010&CampaignId=1102&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
-          }
-          elseif ($origin == 'google'){
-            $form_action = "https://secure.velocify.com/Import.aspx?Provider=ExploreFVI&Client=30010&CampaignId=1101&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
-          }
-        ?>
-        <i class="close-btn fa fa-times-circle" aria-hidden="true"></i>
-        <form method="POST" action="<?= $form_action; ?>">
-            <div class="form-input">
-                <label>Preferred Location</label>
-                <select name="campus" id="campus">
- <!--                	<?php if ($limited != 'yes'){ ?>
-                    <option value="Miami" <?php if ($campus == 'miami' || $campus == '') echo 'selected' ?>>Miami</option>
-                    <?php } ?> -->
-                    <option value="Miramar" <?php if( $campus == 'miramar') echo 'selected' ?>>Miramar</option>
-                    <option value="Online" <?php if ($campus == 'online') echo 'selected' ?>>Online</option>
-                </select>
-            </div>
-            <div class="form-input">
-                <label>Program of Interest</label>
-                <select name="program_name" id="program_name">
-                    <option value="--" disabled <?php if (!isset($program) || $program == '') echo 'selected' ?>>-- Select --</option>
-                    <?php if ($limited != 'yes'){ ?>
-                    <option value="Web Developer" <?php if(isset($program) && $program == 'web-developer') echo 'selected' ?>>Web Developer</option>
-                    <?php } ?>
-                    <option value="Cyber Security Network Technician" <?php if(isset($program) && $program == 'cyber-security') echo 'selected' ?>>Cyber Security and Network Technician</option>
-                    <?php if ($limited != 'yes'){ ?>
-                    <option value="Web Design and Digital Marketing" <?php if(isset($program) && $program == 'web-design-and-digital-marketing') echo 'selected' ?>>Web Design and Digital Marketing</option>
-                    <option value="aws" <?php if(isset($program) && $program == 'aws') echo 'selected' ?>>Amazon Web Services</option>
-                    <!-- <option value="wordpress" <?php if(isset($program) && $program == 'wordpress') echo 'selected' ?>>WordPress</option> -->
-                    <?php } ?>
-                </select>
-                <input type="hidden" name="program_id" id="legacy_program" value="<?php if(isset($program)) echo $program ?>" />
-            </div>
-            <div class="form-input split-2">
-                <div class="split-box">
-                    <label>first name</label>
-                    <input type="text" name="first_name" required>
-                </div>
-                <div class="split-box">
-                    <label>last name</label>
-                    <input type="text" name="last_name" required>
-                </div>
-            </div>
-            <div class="form-input split-2">
-                <div class="split-box">
-                    <label>phone</label>
-                    <input type="tel" name="day_phone" required>
-                </div>
-                <div class="split-box">
-                    <label>zip code</label>
-                    <input type="number" name="zipcode">
-                </div>
-            </div>
-            <div class="form-input">
-                <label>email</label>
-                <input type="email" name="email" required>
-            </div>
-            <button class="submit-btn" type="submit">get info</button>
-            <div class="user-feedback"></div>
-        </form>
-        <p class="disclosure">By clicking "Get Info", you agree for FVI School of Technology to contact you using our autodialer, text, pre-recorded message, via telephone or mobile device (including SMS and MMS) and email, even if your number is currently listed on any state, federal or corporate "Do Not Call list".</p>
-    </div>
+			if ($origin == 'bing'){
+				$form_action = "https://secure.velocify.com/Import.aspx?Provider=YahooBingPPC&Client=30010&CampaignId=1065&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
+			}
+			elseif ($origin == 'es-bing'){
+				$form_action = "https://secure.velocify.com/Import.aspx?Provider=YahooBingPPCSpanish&Client=30010&CampaignId=1083&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
+			}
+			elseif ($origin == 'facebook'){
+				$form_action = "https://secure.velocify.com/Import.aspx?Provider=FacebookAds&Client=30010&CampaignId=1059&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
+			}
+			elseif ($origin == 'coursereport'){
+				$form_action = "https://secure.velocify.com/Import.aspx?Provider=CourseReport&Client=30010&CampaignId=1102&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
+			}
+			elseif ($origin == 'google'){
+				$form_action = "https://secure.velocify.com/Import.aspx?Provider=ExploreFVI&Client=30010&CampaignId=1101&URL=https://www.techlaunch.io/thank-you&referrerpage=" . $escaped_url;
+			}
+		?>
+		<i class="close-btn fa fa-times-circle" aria-hidden="true"></i>
+		<form method="POST" action="<?= $form_action; ?>">
+			<div class="form-input">
+				<label>Preferred Location</label>
+				<select name="campus" id="campus">
+<!--                <?php if ($limited != 'yes'){ ?>
+					<option value="Miami" <?php if ($campus == 'miami' || $campus == '') echo 'selected' ?>>Miami</option>
+					<?php } ?> -->
+					<option value="Miramar" <?php if( $campus == 'miramar') echo 'selected' ?>>Miramar</option>
+					<option value="Online" <?php if ($campus == 'online') echo 'selected' ?>>Online</option>
+				</select>
+			</div>
+			<div class="form-input">
+				<label>Program of Interest</label>
+				<select name="program_name" id="program_name">
+					<option value="--" disabled <?php if (!isset($program) || $program == '') echo 'selected' ?>>-- Select --</option>
+					<?php if ($limited != 'yes'){ ?>
+					<option value="Web Developer" <?php if(isset($program) && $program == 'web-developer') echo 'selected' ?>>Web Developer</option>
+					<?php } ?>
+					<option value="Cyber Security Network Technician" <?php if(isset($program) && $program == 'cyber-security') echo 'selected' ?>>Cyber Security and Network Technician</option>
+					<?php if ($limited != 'yes'){ ?>
+					<option value="Web Design and Digital Marketing" <?php if(isset($program) && $program == 'web-design-and-digital-marketing') echo 'selected' ?>>Web Design and Digital Marketing</option>
+					<option value="aws" <?php if(isset($program) && $program == 'aws') echo 'selected' ?>>Amazon Web Services</option>
+					<!-- <option value="wordpress" <?php if(isset($program) && $program == 'wordpress') echo 'selected' ?>>WordPress</option> -->
+					<?php } ?>
+				</select>
+				<input type="hidden" name="program_id" id="legacy_program" value="<?php if(isset($program)) echo $program ?>" />
+			</div>
+			<div class="form-input split-2">
+				<div class="split-box">
+					<label>first name</label>
+					<input type="text" name="first_name" required>
+				</div>
+				<div class="split-box">
+					<label>last name</label>
+					<input type="text" name="last_name" required>
+				</div>
+			</div>
+			<div class="form-input split-2">
+				<div class="split-box">
+					<label>phone</label>
+					<input type="tel" name="day_phone" required>
+				</div>
+				<div class="split-box">
+					<label>zip code</label>
+					<input type="number" name="zipcode">
+				</div>
+			</div>
+			<div class="form-input">
+				<label>email</label>
+				<input type="email" name="email" required>
+			</div>
+			<div class="form-input">
+				<label for="custom_field_8">How Did You Hear About Us</label>
+				<select name="custom_field_8" id="custom_field_8">
+					<option value="event">Event</option>
+					<option value="facebook">Facebook</option>
+					<option value="friend-family">Friend/Family Member</option>
+					<option value="google">Google</option>
+					<option value="hulu">Hulu</option>
+					<option value="instagram">Instagram</option>
+					<option value="pinterest">Pinterest</option>
+					<option value="radio">Radio</option>
+					<option value="tv">TV</option>
+					<option value="twitter">Twitter</option>
+					<option value="youtube">YouTube</option>
+					<option value="other">Other</option>
+				</select>
+			</div>
+			<button class="submit-btn" type="submit">get info</button>
+			<div class="user-feedback"></div>
+		</form>
+		<p class="disclosure">By clicking "Get Info", you agree for FVI School of Technology to contact you using our autodialer, text, pre-recorded message, via telephone or mobile device (including SMS and MMS) and email, even if your number is currently listed on any state, federal or corporate "Do Not Call list".</p>
+	</div>
 </div>
 
-
 <script>
-    document.getElementById("program_name").onchange = function(){
-        document.getElementById("legacy_program").value = document.getElementById("program_name").value;
-    }
+	document.getElementById("program_name").onchange = function(){
+		document.getElementById("legacy_program").value = document.getElementById("program_name").value;
+	}
 </script>
